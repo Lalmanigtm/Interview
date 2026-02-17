58c080d889204410ca869b896bb3adce4202f33f
Saurab Gautam <missionprojects250@gmail.com>
2026-02-17 13:15:08 +0545

    deployment config added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,29 @@
  
  import express from "express";
+ import path from "path";
  import { ENV } from "./lib/env.js";
 
  const app = express();
- console.log(ENV.PORT)
- console.log(ENV.DB_URL)
+//  console.log(ENV.PORT)
+//  console.log(ENV.DB_URL)
+const __dirname = path.resolve();
 
  app.use("/health", (req, res) =>{
   res.status(200).json({msg: "Welcome to the Home Page"})
  });
+ app.use("/books", (req, res) =>{
+  res.status(200).json({msg: "Welcome to the BOOKING"})
+ });
+
+// Make our app ready for deployment
+if(ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("/{*any}", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+
+}
 
  app.listen(ENV.PORT, () =>{
   console.log("Welcome to the world of New: ",ENV.PORT)
