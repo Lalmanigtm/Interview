15b08b3b1a9a43d8d74ae11354095ddea59a174d
Saurab Gautam <missionprojects250@gmail.com>
2026-02-18 18:38:28 +0545

    pushed new

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,12 +21,13 @@ const __dirname = path.resolve();
 if(ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("/{*any}", (req, res) =>{
+  app.get("*", (req, res) =>{
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   })
 
 }
 
+if(ENV.NODE_ENV != "production")
  app.listen(ENV.PORT, () =>{
   console.log("Welcome to the world of New: ",ENV.PORT)
  })
